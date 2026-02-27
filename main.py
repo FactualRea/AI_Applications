738a10bd56345dd4e4d1f98c06c04ea75a433198
Reatlegile <rmatsepe30@gmail.com>
2026-02-27 13:46:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from ai-applications!")
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
 
+if not api_key:
+    raise ValueError("OPENAI_API_KEY not found in env file")
 
-if __name__ == "__main__":
-    main()
+#print("Loaded key:", api_key[:4] + "…")
+
+def get_response(prompt):
+    client = OpenAI(api_key = api_key)
+    response = client.chat.completions.create(
+        model="gpt-4o-mini", max_completion_tokens=100,
+        messages=[{"role": "user", "content": prompt}]
+    )
+    return response.choices[0].message.content
+
+response = get_response("explain about different role, user, system and use bullet point for your response")
+print(response)
